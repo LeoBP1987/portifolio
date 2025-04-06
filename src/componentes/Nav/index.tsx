0a5b877318950b02aeafa3f1c8bab35a667ae78a
LeoBP1987 <leonardobp1987@gmail.com>
2025-04-06 16:30:02 -0300

    Fix: Ajustando Responsividade Cabeçalho v5

--- src/componentes/Nav/index.tsx
+++ src/componentes/Nav/index.tsx
@@ -29,7 +29,7 @@ const NavEstilizado = styled.nav<{ $displayMobile: string }>`
         z-index: 1000;
         width: 50%;
         position: absolute;
-        top: 20px;
+        top: 50px;
         left: 130px;
         background: var(--cor-primaria);
         flex-direction: column;
