556a70027bfea8c8d7dbb3d625e211d7b989c1d0
LeoBP1987 <leonardobp1987@gmail.com>
2025-04-06 17:26:23 -0300

    Fix: Ajustando Margem de repsonsividade texto sobre mim do cabeçalho

--- src/componentes/Cabecalho/index.tsx
+++ src/componentes/Cabecalho/index.tsx
@@ -53,9 +53,8 @@ const ParagrafoEstilizado = styled.p`
     text-align: justify;
     line-height: 1.1rem;
     @media screen and (max-width: 800px) {
-        margin: 0 20px;
-        width: 75%;
-        font-size: 0.7rem;
+        width: 60%;
+        font-size: 0.65rem;
     }
 `
 
