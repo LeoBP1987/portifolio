f533e513b1ccbb65d8de33da59729420933f48b2
LeoBP1987 <leonardobp1987@gmail.com>
2025-05-22 19:40:25 -0300

    Perf: Aumentando espaço entre as tecnologias

--- src/componentes/MinhasTecnologias/index.tsx
+++ src/componentes/MinhasTecnologias/index.tsx
@@ -104,7 +104,7 @@ const ListaTecnologias = styled.div`
     width: 600px;
     display: flex;
     flex-wrap: wrap;
-    gap: 5px;
+    gap: 32px;
     justify-content: flex-start;
     align-items: center;
     padding: 0;
@@ -119,14 +119,14 @@ const ListaTecnologias = styled.div`
 
 const ItemTecnologia = styled.div`
     margin: 0;
-    padding: 15px;
+    padding: 20px;
     width: 130px;
     height: 81.25px;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: space-between;
-    gap: 5px;
+    gap: 10px;
     background: var(--cor-primaria);
     img {
         margin: 0;
